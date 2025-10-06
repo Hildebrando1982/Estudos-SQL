20c9695ef211bde9d5aad6a0f697e4df28c7856e
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-06 08:40:36 -0300

    Create Operadpres_Relacionais_e_Lógicos.sql
    Operadores Relacionais e Operadores Lógicos no SQL

---
+++ Operadpres_Relacionais_e_Lógicos.sql
@@ -0,0 +1,20 @@
+''' Operadores Relacionais no SQL
+ = igual
+ < menor
+ <= menor ou igual
+ > maior
+ >= maior ou igual
+ != ou <> diferente
+
+ Operadores Lógicos (booleano Verdadeiro ou Falso)
+ and e
+ or ou
+ not ou ! não
+
+Operadores especiais
+es null / is not null verifica a nulidade do campo
+Between Determina um intervalo para a condição. podendo ser substiuido por >= e <=
+Like Utilizado para a comparação do texto
+IN Compara o valor 
+
+ '''
